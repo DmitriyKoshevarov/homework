0e36040f9b24902d444a2f91b172abf9e2dc359c
ÐDmitriy <dimakoshevarov1@gmail.com>
2024-11-17 12:42:32 +0300

    master1

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,15 @@
-homeworks_quantity = 12
-hours_spent = 1.5
-course_name = 'Python'
-time_for_one_task = hours_spent / homeworks_quantity
-print('Курс:',course_name,', всего задач:',homeworks_quantity,', затрачено часов:',hours_spent,', среднее время выполнения',time_for_one_task,'часа.')
-print(f"Курс: {course_name}, всего задач: {homeworks_quantity}, затрачено часов: {hours_spent}, среднее время выполнения {time_for_one_task} часа.")
+my_string = input('Введите текст: ')
+#количество символов с учетом пробелов
+print(len(my_string))
+#количество символов без учета пробелов
+print(len(my_string.replace(' ','')))
+#вывод строки в верхнем регистре
+print(my_string.upper())
+#вывод строки в нижнем регистре
+print(my_string.lower())
+#вывод строки без пробелов
+print(my_string.replace(' ',''))
+#вывод первого символа строки
+print(my_string[0])
+#вывод последнего символа строки
+print(my_string[-1])
