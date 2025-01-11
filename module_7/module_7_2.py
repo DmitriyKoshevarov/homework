74254f7f683d731e6dcc4dea216676d9253f4f29
DmitriyKoshevarov <dimakoshevarov1@gmail.com>
2025-01-11 17:52:46 +0300

    first

---
+++ module_7/module_7_2.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    file = open(file_name, 'w', encoding='utf-8')
+    for index, string in enumerate(strings, start=1):
+        byte_position = file.tell()
+        file.write(f'{string}\n')
+        strings_positions[(index, byte_position)] = string
+    return strings_positions
+    file.close()
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+
+for elem in result.items():
+  print(elem)
