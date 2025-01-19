08bd3e09e39a9b3a3ed0ffa113f24af3bac16048
DmitriyKoshevarov <dimakoshevarov1@gmail.com>
2025-01-19 22:16:40 +0300

    first

--- module_6/module_6_extra.py
+++ module_6/module_6_extra.py
@@ -1,10 +1,12 @@
+from math import pi
+
 class Figure:
+    sides_count = 0
 
     def __init__(self,__color = (0,0,0), *sides):
         self.__color = __color
         self.__sides = list(sides)
         self.filled = False
-        self.sides_count = len(sides)
 
     def get_color(self):
         return self.__color
@@ -30,12 +32,60 @@ class Figure:
     def get_sides(self):
         return self.__sides
 
-circle1 = Figure((200, 200, 100), 10)
-circle1.set_sides(5, 3, 12, 4, 5) # Не изменится
+    def __len__(self, *sides):
+        return sum(self.__sides)
 
-print(circle1.get_sides())
+class Circle(Figure):
+    sides_count = 1
 
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+        self.__radius = self.__len__()/2*pi
+
+    def get_square(self, __radius):
+        return pi(*__radius**2)
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+
+    def get_square(self):
+        a, b, c = self.get_sides()
+        s = (a + b + c) / 2
+        return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+        self.set_sides(*[sides[0]] * self.sides_count)
+
+    def get_volume(self):
+        a = self.get_sides()[0]
+        return a**3
+
+
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
 circle1.set_sides(15) # Изменится
-
 print(circle1.get_sides())
 
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+# Проверка объёма (куба):
+print(cube1.get_volume())
--- module_7/explorer.py
+++ module_7/explorer.py
@@ -1,13 +1,25 @@
 import tkinter
+import os
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile, askopenfilename
+
+
+def file_select():
+    filename = askopenfilename(initialdir=".", title="Выберите файл",
+                               filetypes=(('Текстовый файл', '.txt'),
+                                          ('Все файлы','*')))
+    text['text'] = text['text'] + ' ' + filename
+    os.startfile(filename)
 
 window = tkinter.Tk()
 window.title('Проводник')
-window.geometry('350x350')
-window.configure(bg='green')
+window.geometry('450x150')
+window.configure(bg='black')
 window.resizable(False,False)
-text = tkinter.Label(window, text='Файл:', width=20,height=5, background='silver')
+text = tkinter.Label(window, text='Файл:', width=65, height=5, background='silver', foreground='blue')
 text.grid(column=1, row=1)
-button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл')
-button_select.grid(column=1, row=2)
+button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл',
+                               background='silver', foreground='blue',
+                               command=file_select)
+button_select.grid(column=1, row=2, pady = 5)
 window.mainloop()
-
